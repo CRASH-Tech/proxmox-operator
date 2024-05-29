d03cbbe1a8d806aff3b4f125c399836997aabb7a
s.kharlamov <s.kharlamov@comagic.dev>
2024-05-29 09:50:29 +0000

    add license

--- cmd/proxmox/client.go
+++ cmd/proxmox/client.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2024 The CRASH-Tech.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package proxmox
 
 import (
@@ -36,7 +52,7 @@ func NewClient(clusters map[string]ClusterApiConfig) *Client {
 func (client *Client) Cluster(cluster string) *Cluster {
 	apiConfig, isExists := client.Clusters[cluster]
 	if !isExists {
-		log.Error("unknown cluster: %s", cluster)
+		log.Errorf("unknown cluster: %s", cluster)
 	}
 
 	restyClient := resty.New()
